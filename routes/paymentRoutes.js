f9beec7c719990843cdc60bcfa3a2edd1f3c2aea
Mark Simpson <mark.simpson4@gmail.com>
2025-04-05 16:28:52 +0100

    body parser added

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const paymentController = require('../controllers/paymentController')
+const bodyParser = require('body-parser')
 
 router
   .route('/create-payment-session')
@@ -13,7 +14,7 @@ router.route('/get-tokens/:id').get(paymentController.getTokenCount)
 router
   .route('/webhook')
   .post(
-    express.raw({ type: 'application/json' }),
+    bodyParser.raw({ type: 'application/json' }),
     paymentController.successfulPayment
   )
 
