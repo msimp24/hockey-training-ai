7a3f13c811ac8cae3c83d1e126f1a3a4ae0737f9
Mark Simpson <mark.simpson4@gmail.com>
2025-04-05 17:29:14 +0100

    troubleshooting

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const path = require('path')
-const bodyParser = require('body-parser')
 
 //routers
 const authRouter = require('./routes/authRoute')
@@ -16,7 +15,6 @@ const paymentRouter = require('./routes/paymentRoutes')
 
 const app = express()
 app.use(express.json())
-app.use(bodyParser.json())
 
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const paymentController = require('../controllers/paymentController')
-const bodyParser = require('body-parser')
 
 router
   .route('/create-payment-session')
@@ -14,7 +13,7 @@ router.route('/get-tokens/:id').get(paymentController.getTokenCount)
 router
   .route('/webhook')
   .post(
-    bodyParser.raw({ type: 'application/json' }),
+    express.raw({ type: 'application/json' }),
     paymentController.successfulPayment
   )
 
